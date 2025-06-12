0b07da49b718173a643efb1ea25048fc7e742c1d
Camille <camilledubois1995@gmail.com>
2025-06-12 19:29:34 -0400

    implemented scoreWord and highestScoreFrom

--- src/adagrams.js
+++ src/adagrams.js
@@ -57,9 +57,67 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  let points = 0;
+
+  if (word.length >= 7) {
+    points += 8;
+  }
+
+  for (const char of word) {
+    const charUpper = char.toUpperCase();
+    if (['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'].includes(charUpper)) {
+      points += 1;
+    } else if (['D', 'G'].includes(charUpper)) {
+      points += 2;
+    } else if (['B', 'C', 'M', 'P'].includes(charUpper)) {
+      points += 3;
+    } else if (['F', 'H', 'V', 'W', 'Y'].includes(charUpper)) {
+      points += 4;
+    } else if (charUpper === 'K') {
+      points += 5;
+    } else if (['J', 'X'].includes(charUpper)) {
+      points += 8;
+    } else {
+      points += 10;
+    }
+  }
+  return points;
 };
 
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 4
+  let wordScores = {};
+  for (const word of words) {
+    wordScores[word] = scoreWord(word);
+  }
+
+  let winningScore = Object.values(wordScores)[0];
+  for (const score of Object.values(wordScores)) {
+    if (score > winningScore) {
+      winningScore = score;
+    }
+  }
+
+  let winningWords = [];
+  for (const word in wordScores) {
+    if (wordScores[word]=== winningScore) {
+      winningWords.push(word);
+    }
+  }
+
+  let winningWord;
+  if (winningWords.length > 1) {
+    winningWord = winningWords[0]
+    for (const word of winningWords) {
+      if (word.length >= 10) {
+        return { word, score: winningScore };
+      } else if (word.length < winningWord.length) {
+        winningWord = word;
+      } 
+    }
+  } else {
+    return { word: winningWords[0], score: winningScore };
+  }
+  return { word: winningWord, score: winningScore };
 };
+
+
